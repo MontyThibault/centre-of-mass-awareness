7616d13d94d3a1bfae51132725b5b816ac11b200
MontyThibault <montythibault@gmail.com>
2016-06-28 11:43:46 -0400

    refactored loadHelper

--- forcePlates/MayaIntegration/tests/sixaxis/test_sixaxis_calibration.py
+++ forcePlates/MayaIntegration/tests/sixaxis/test_sixaxis_calibration.py
@@ -3,5 +3,24 @@ import unittest
 
 class TestSixAxisCalibration(unittest.TestCase):
 
+	def test_calibrate_matrix_test_and_save(self):
+
+		x = SixAxisCalibrationMatrix()
+
+		assert x.process([1, 2, 3, 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
+		x.matrix[2][3] = 1
+		assert x.process([0, 0, 0, 1, 0, 0]) == [0, 0, 1, 1, 0, 0]
+
+		x.name = 'test'
+		x.save()
+
+		x.matrix[2][3] = 0
+
+		x.load()
+		assert x.process([0, 0, 0, 1, 0, 0]) == [0, 0, 1, 1, 0, 0]
+
+		x.delete()
+
 	def test_insert_factory_six_axis_calibrations(self):
 		SixAxisCalibrationMatrix.insert_factory_six_axis_calibrations()
--- forcePlates/MayaIntegration/tests/test_load_helper.py
+++ forcePlates/MayaIntegration/tests/test_load_helper.py
@@ -1,4 +1,4 @@
-from plugin.load_helper import LoadHelper
+import plugin.load_helper as lh
 import unittest
 
 
@@ -6,28 +6,8 @@ class TestLoadHelper(unittest.TestCase):
 
 	def test_save_and_delete_new_persistent_entry(self):
 
-		LoadHelper.save('test', hash(self))
-		assert LoadHelper.load('test') == hash(self)
+		lh.save('test', hash(self))
+		assert lh.load('test') == hash(self)
 
-		LoadHelper.delete('test')
-		assert not LoadHelper.exists('test')
-
-
-	def test_calibrate_matrix_test_and_save(self):
-
-		x = SixAxisCalibrationMatrix()
-
-		assert x.process([1, 2, 3, 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
-
-		x.matrix[2][3] = 1
-		assert x.process([0, 0, 0, 1, 0, 0]) == [0, 0, 1, 1, 0, 0]
-
-		x.name = 'test'
-		x.save()
-
-		x.matrix[2][3] = 0
-
-		x.load()
-		assert x.process([0, 0, 0, 1, 0, 0]) == [0, 0, 1, 1, 0, 0]
-
-		x.delete()
+		lh.delete('test')
+		assert not lh.exists('test')
